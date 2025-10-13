45d1daa2f7e09d9d4f46d8823a4e2436d7d4c92f
Justin S <justinsuiter4@gmail.com>
2025-10-13 08:36:40 -0400

    Added Damping system and fixed forced rotation.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -42,7 +42,9 @@ public class Movement : MonoBehaviour
     }
     private void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
     private void ProcessThrust()
     {
